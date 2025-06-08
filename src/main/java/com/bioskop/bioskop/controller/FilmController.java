d1444f99540c620a3f1e0d8f9900bd31dbc445b6
JeffryVisnu <safridaida1@gmail.com>
2025-06-08 18:29:42 +0700

    Tambah WebConfig

--- src/main/java/com/bioskop/bioskop/controller/FilmController.java
+++ src/main/java/com/bioskop/bioskop/controller/FilmController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.bioskop.bioskop.model.Film;
 import com.bioskop.bioskop.repository.FilmRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/film")
 public class FilmController {
