2a19f8f6f5bf03dc0eff0ac81a4c13bbd6059d76
Shadra <irfanshadra7@gmail.com>
2025-06-13 21:50:12 +0700

    menambahkan endpoint get untuk mendapatkan informasi jadwal berdasarkan idFilm dan idBioskop

--- src/main/java/com/bioskop/bioskop/controller/JadwalController.java
+++ src/main/java/com/bioskop/bioskop/controller/JadwalController.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.bioskop.bioskop.model.Jadwal;
 import com.bioskop.bioskop.model.Kursi;
+import com.bioskop.bioskop.repository.BioskopRepository;
+import com.bioskop.bioskop.repository.FilmRepository;
 import com.bioskop.bioskop.repository.JadwalRepository;
 
 @RestController
@@ -17,6 +22,12 @@ public class JadwalController {
     @Autowired
     private JadwalRepository jadwalRepository;
 
+    @Autowired
+    private FilmRepository filmRepository;
+
+    @Autowired
+    private BioskopRepository bioskopRepository;
+
     @GetMapping("/{id}/kursi-tersedia")
     public List<Kursi> getKursiTersedia(@PathVariable String id) {
         Jadwal jadwal = jadwalRepository.findById(id)
@@ -25,5 +36,10 @@ public class JadwalController {
         return jadwal.getKursiList().stream()
             .filter(Kursi::isTersedia)
             .collect(Collectors.toList());
+    }
+
+    @GetMapping("/film/{idFilm}/bioskop/{idBioskop}")
+    public List<Jadwal> getJadwalByFilmAndBioskop(@PathVariable String idFilm, @PathVariable String idBioskop) {
+        return jadwalRepository.findByFilm_IdFilmAndBioskop_IdBioskop(idFilm, idBioskop);
     }
 }
--- src/main/java/com/bioskop/bioskop/repository/JadwalRepository.java
+++ src/main/java/com/bioskop/bioskop/repository/JadwalRepository.java
@@ -1,10 +1,12 @@
 package com.bioskop.bioskop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bioskop.bioskop.model.Jadwal;
 @Repository
 public interface JadwalRepository extends JpaRepository<Jadwal, String>{
-
+    List<Jadwal> findByFilm_IdFilmAndBioskop_IdBioskop(String idFilm, String idBioskop);
 }
